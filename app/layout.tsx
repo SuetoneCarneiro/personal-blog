7f0b7ff732a8cc246993bf8fdf30df5561af8a63
SuetoneCarneiro <suetoneneto12@gmail.com>
2026-02-18 11:44:26 -0300

    chore: configure vercel analytics

---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from '@vercel/analytics/next';
+import "./globals.css";
+import { Navbar } from '../components/navbar';
+import { Footer } from '../components/footer';
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Suetone Carneiro | Website",
+  description: "Personal Website and portfolio of a Software Developer",
+  openGraph:{
+    title: "Suetone Carneiro | Website",
+    description: "Website pessoal e portfólio",
+    images: ['/home.png',]
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navbar />
+        <div className="flex-grow">
+          {children}
+        </div>
+        <Footer />
+        <Analytics />
+      </body>
+    </html>
+  );
+}
